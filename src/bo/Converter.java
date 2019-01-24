47b96c45178abec141e0a092e192858178c2a1e8
Florin Barbisch <florin.barbisch@gmail.com>
2019-01-24 20:12:18 +0100

    Changed from Long to BigDecimal for dealing with bigger numbers

--- src/bo/Converter.java
+++ src/bo/Converter.java
@@ -1,11 +1,38 @@
 package bo;
 
+import java.math.BigInteger;
+
+/**
+ * A class which currently only contains one method for converting a number to a
+ * number with an other radix.
+ * 
+ * @author Florin Barbisch
+ *
+ */
 public class Converter {
-	public static String convert(String value, int inputDigits, int outputDigits){
-        try {
-            return Long.toString(Long.parseLong(value, inputDigits), outputDigits);
-        } catch (NumberFormatException e) {
-            return null;
-        }
+	
+	/**
+	 * Converts a String with a specific radix to another number with a specific
+	 * radix. Returns {@code null} if one of both radix are not inside the range
+	 * from {@link Character#MIN_RADIX} to {@link Character#MAX_RADIX} inclusive, or
+	 * value is not a valid representation of a {@link BigInteger}.
+	 * 
+	 * @param value       A {@link String} representing a {@link BigInteger}
+	 * @param inputRadix  The radix of the input value
+	 * @param outputRadix The radix of the output value
+	 * @return {@link String} representation of the given value (and it's given
+	 *         radix) in the given radix.
+	 * 
+	 * @see {@link BigInteger#BigInteger(String, int)}
+	 * @see {@link BigInteger#toString(int)}
+	 */
+	public static String convert(String value, int inputRadix, int outputRadix){
+		if (inputRadix < Character.MIN_RADIX || inputRadix > Character.MAX_RADIX || outputRadix < Character.MIN_RADIX || outputRadix > Character.MAX_RADIX) {
+			return null;
+		}
+		
+		try {
+			return new BigInteger(value, inputRadix).toString(outputRadix);
+        } catch (NumberFormatException e) { return null; }
     }
 }
