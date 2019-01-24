db0c1cbe6ae5a8a531db9fc0ba34bafaf1af728a
Florin Barbisch <florin.barbisch@gmail.com>
2019-01-24 18:02:28 +0100

    Changed to an eclipse project

--- src/bo/Converter.java
+++ src/bo/Converter.java
@@ -1,11 +1,11 @@
 package bo;
 
 public class Converter {
-    public static String convert(String value, int inputDigits, int outputDigits){
+	public static String convert(String value, int inputDigits, int outputDigits){
         try {
             return Long.toString(Long.parseLong(value, inputDigits), outputDigits);
         } catch (NumberFormatException e) {
-            return "";
+            return null;
         }
     }
 }
--- src/bo/Main.java
+++ src/bo/Main.java
@@ -1,12 +1,12 @@
 package bo;
 
+import java.awt.EventQueue;
+
 import ui.ConverterInterface;
 
 public class Main {
 
     public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> {
-            new ConverterInterface().setVisible(true);
-        });
+    	EventQueue.invokeLater(() -> { new ConverterInterface().setVisible(true); });
     }
 }
